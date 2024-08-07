a8fa783379df79fa186a7ff8918f7a9acecb6ac2
Mareks <47260097+sshsphere@users.noreply.github.com>
2024-08-07 23:16:52 +0300

    .

--- cmake/FetchDawn.cmake
+++ cmake/FetchDawn.cmake
@@ -1,205 +1,111 @@
 # Prevent multiple includes
 if (TARGET dawn_native)
-	return()
+    return()
 endif()
 
 include(FetchContent)
 
 FetchContent_Declare(
-	dawn
-	DOWNLOAD_COMMAND
-		cd ${FETCHCONTENT_BASE_DIR}/dawn-src &&
-		git init &&
-		git fetch --depth=1 https://dawn.googlesource.com/dawn chromium/6536 &&
-		git reset --hard FETCH_HEAD
+    dawn
+    DOWNLOAD_COMMAND
+        cd ${FETCHCONTENT_BASE_DIR}/dawn-src &&
+        git init &&
+        git fetch --depth=1 https://dawn.googlesource.com/dawn chromium/6536 &&
+        git reset --hard FETCH_HEAD
 
-	PATCH_COMMAND
-		"${CMAKE_COMMAND}" -E copy
-		"${CMAKE_CURRENT_LIST_DIR}/../patch/tools/fetch_dawn_dependencies.py"
-		tools
+    PATCH_COMMAND
+        "${CMAKE_COMMAND}" -E copy
+        "${CMAKE_CURRENT_LIST_DIR}/../patch/tools/fetch_dawn_dependencies.py"
+        tools
 )
 
 FetchContent_GetProperties(dawn)
 if (NOT dawn_POPULATED)
-	FetchContent_Populate(dawn)
+    FetchContent_Populate(dawn)
 
-	# This option replaces depot_tools
-	set(DAWN_FETCH_DEPENDENCIES ON)
+    # This option replaces depot_tools
+    set(DAWN_FETCH_DEPENDENCIES ON)
 
-	# A more minimalistic choice of backand than Dawn's default
-	if (APPLE)
-		set(USE_VULKAN OFF)
-		set(USE_METAL ON)
-	else()
-		set(USE_VULKAN ON)
-		set(USE_METAL OFF)
-	endif()
-	set(DAWN_ENABLE_D3D11 OFF CACHE BOOL "Enable compilation of the D3D11 backend")
-	set(DAWN_ENABLE_D3D12 OFF CACHE BOOL "Enable compilation of the D3D12 backend")
-	set(DAWN_ENABLE_METAL ${USE_METAL} CACHE BOOL "Enable compilation of the Metal backend")
-	set(DAWN_ENABLE_NULL OFF CACHE BOOL "Enable compilation of the Null backend")
-	set(DAWN_ENABLE_DESKTOP_GL OFF CACHE BOOL "Enable compilation of the OpenGL backend")
-	set(DAWN_ENABLE_OPENGLES OFF CACHE BOOL "Enable compilation of the OpenGL ES backend")
-	set(DAWN_ENABLE_VULKAN ${USE_VULKAN} CACHE BOOL "Enable compilation of the Vulkan backend")
-	set(TINT_BUILD_SPV_READER OFF CACHE BOOL "Build the SPIR-V input reader")
-	if(${DAWN_ENABLE_D3D11} OR ${DAWN_ENABLE_D3D12})
-		set(TINT_BUILD_HLSL_WRITER ON CACHE BOOL "Build the HLSL output writer" FORCE)
-	endif()
-	if(${DAWN_ENABLE_DESKTOP_GL} OR ${DAWN_ENABLE_OPENGLES})
-		set(TINT_BUILD_GLSL_WRITER ON CACHE BOOL "Build the GLSL output writer" FORCE)
-	endif()
-	if(${DAWN_ENABLE_VULKAN})
-		set(TINT_BUILD_SPV_WRITER ON CACHE BOOL "Build the SPIR-V output writer" FORCE)
-	endif()
-	if(${DAWN_ENABLE_METAL})
-		set(TINT_BUILD_MSL_WRITER ON CACHE BOOL "Build the MSL output writer" FORCE)
-	endif()
+    # A more minimalistic choice of backend than Dawn's default
+    if (APPLE)
+        set(USE_VULKAN OFF)
+        set(USE_METAL ON)
+    else()
+        set(USE_VULKAN ON)
+        set(USE_METAL OFF)
+    endif()
+    set(DAWN_ENABLE_D3D11 OFF CACHE BOOL "Enable compilation of the D3D11 backend")
+    set(DAWN_ENABLE_D3D12 OFF CACHE BOOL "Enable compilation of the D3D12 backend")
+    set(DAWN_ENABLE_METAL ${USE_METAL} CACHE BOOL "Enable compilation of the Metal backend")
+    set(DAWN_ENABLE_NULL OFF CACHE BOOL "Enable compilation of the Null backend")
+    set(DAWN_ENABLE_DESKTOP_GL OFF CACHE BOOL "Enable compilation of the OpenGL backend")
+    set(DAWN_ENABLE_OPENGLES OFF CACHE BOOL "Enable compilation of the OpenGL ES backend")
+    set(DAWN_ENABLE_VULKAN ${USE_VULKAN} CACHE BOOL "Enable compilation of the Vulkan backend")
+    set(TINT_BUILD_SPV_READER OFF CACHE BOOL "Build the SPIR-V input reader")
+    if(${DAWN_ENABLE_D3D11} OR ${DAWN_ENABLE_D3D12})
+        set(TINT_BUILD_HLSL_WRITER ON CACHE BOOL "Build the HLSL output writer" FORCE)
+    endif()
+    if(${DAWN_ENABLE_DESKTOP_GL} OR ${DAWN_ENABLE_OPENGLES})
+        set(TINT_BUILD_GLSL_WRITER ON CACHE BOOL "Build the GLSL output writer" FORCE)
+    endif()
+    if(${DAWN_ENABLE_VULKAN})
+        set(TINT_BUILD_SPV_WRITER ON CACHE BOOL "Build the SPIR-V output writer" FORCE)
+    endif()
+    if(${DAWN_ENABLE_METAL})
+        set(TINT_BUILD_MSL_WRITER ON CACHE BOOL "Build the MSL output writer" FORCE)
+    endif()
 
-	# Disable unneeded parts
-	set(DAWN_BUILD_SAMPLES OFF CACHE BOOL "Enables building Dawn's samples")
-	set(TINT_BUILD_TINTD OFF CACHE BOOL "Build the WGSL language server")
-	set(TINT_BUILD_TESTS OFF CACHE BOOL "Build tests")
-	set(TINT_BUILD_FUZZERS OFF CACHE BOOL "Build fuzzers")
-	set(TINT_BUILD_AST_FUZZER OFF CACHE BOOL "Build AST fuzzer")
-	set(TINT_BUILD_REGEX_FUZZER OFF CACHE BOOL "Build regex fuzzer")
-	set(TINT_BUILD_IR_FUZZER OFF CACHE BOOL "Build IR fuzzer")
-	set(TINT_BUILD_BENCHMARKS OFF CACHE BOOL "Build Tint benchmarks")
-	set(TINT_BUILD_AS_OTHER_OS OFF CACHE BOOL "Override OS detection to force building of *_other.cc files")
+    # Disable unneeded parts
+    set(DAWN_BUILD_SAMPLES OFF CACHE BOOL "Enables building Dawn's samples")
+    set(TINT_BUILD_TINTD OFF CACHE BOOL "Build the WGSL language server")
+    set(TINT_BUILD_TESTS OFF CACHE BOOL "Build tests")
+    set(TINT_BUILD_FUZZERS OFF CACHE BOOL "Build fuzzers")
+    set(TINT_BUILD_AST_FUZZER OFF CACHE BOOL "Build AST fuzzer")
+    set(TINT_BUILD_REGEX_FUZZER OFF CACHE BOOL "Build regex fuzzer")
+    set(TINT_BUILD_IR_FUZZER OFF CACHE BOOL "Build IR fuzzer")
+    set(TINT_BUILD_BENCHMARKS OFF CACHE BOOL "Build Tint benchmarks")
+    set(TINT_BUILD_AS_OTHER_OS OFF CACHE BOOL "Override OS detection to force building of *_other.cc files")
 
-	add_subdirectory(${dawn_SOURCE_DIR} ${dawn_BINARY_DIR} EXCLUDE_FROM_ALL)
+    add_subdirectory(${dawn_SOURCE_DIR} ${dawn_BINARY_DIR} EXCLUDE_FROM_ALL)
 
-    # Correctly set interface sources
-    set_target_properties(dawn_native PROPERTIES
-        INTERFACE_SOURCES ""
+    # Ensure all necessary targets are added to the export set
+    set(AllDawnTargets
+        core_tables
+        dawn_common
+        dawn_glfw
+        dawn_headers
+        dawn_native
+        dawn_platform
+        dawn_proc
+        dawn_utils
+        dawn_wire
+        dawncpp
+        dawncpp_headers
+        emscripten_bits_gen
+        enum_string_mapping
+        extinst_tables
+        webgpu_dawn
+        webgpu_headers_gen
+        partition_alloc
+        dawn_internal_config
+        tint_api
+        SPIRV-Tools-opt
     )
-    set_target_properties(dawn_headers PROPERTIES
-        INTERFACE_SOURCES ""
-    )
+
+    foreach (Target ${AllDawnTargets})
+        if (TARGET ${Target})
+            # Is a target...
+            get_property(AliasedTarget TARGET "${Target}" PROPERTY ALIASED_TARGET)
+            if("${AliasedTarget}" STREQUAL "")
+                # ...and is not an alias -> move to the Dawn folder
+                set_property(TARGET ${Target} PROPERTY FOLDER "Dawn")
+                # Add to export set
+                install(TARGETS ${Target} EXPORT webgpu-export)
+            endif()
+        endif()
+    endforeach()
+
+    # This is likely needed for other targets as well
+    # TODO: Notify this upstream (is this still needed?)
+    target_include_directories(dawn_utils PUBLIC "${CMAKE_BINARY_DIR}/_deps/dawn-src/src")
 endif()
-
-set(AllDawnTargets
-	core_tables
-	dawn_common
-	dawn_glfw
-	dawn_headers
-	dawn_native
-	dawn_platform
-	dawn_proc
-	dawn_utils
-	dawn_wire
-	dawncpp
-	dawncpp_headers
-	emscripten_bits_gen
-	enum_string_mapping
-	extinst_tables
-	webgpu_dawn
-	webgpu_headers_gen
-	
-	tint-format
-	tint-lint
-	tint_api
-	tint_api_common
-	tint_api_options
-	tint_cmd_common
-	tint_cmd_info_cmd
-	tint_cmd_loopy_cmd
-	tint_cmd_remote_compile_cmd
-	tint_cmd_tint_cmd
-	tint_lang_core
-	tint_lang_core_constant
-	tint_lang_core_intrinsic
-	tint_lang_core_ir
-	tint_lang_core_ir_transform
-	tint_lang_core_ir_transform_common
-	tint_lang_core_type
-	tint_lang_glsl_writer
-	tint_lang_glsl_writer_common
-	tint_lang_glsl_writer_printer
-	tint_lang_glsl_writer_ast_printer
-	tint_lang_glsl_writer_ast_raise
-	tint_lang_glsl_writer_raise
-	tint_lang_glsl_validate
-	tint_lang_hlsl_writer_common
-	tint_lang_hlsl_writer_helpers
-	tint_lang_msl
-	tint_lang_msl_intrinsic
-	tint_lang_msl_ir
-	tint_lang_msl_writer_raise
-	tint_lang_spirv
-	tint_lang_spirv_intrinsic
-	tint_lang_spirv_ir
-	tint_lang_spirv_reader_common
-	tint_lang_spirv_reader_lower
-	tint_lang_spirv_type
-	tint_lang_spirv_validate
-	tint_lang_spirv_writer
-	tint_lang_spirv_writer_ast_printer
-	tint_lang_spirv_writer_ast_raise
-	tint_lang_spirv_writer_common
-	tint_lang_spirv_writer_helpers
-	tint_lang_spirv_writer_printer
-	tint_lang_spirv_writer_raise
-	tint_lang_wgsl
-	tint_lang_wgsl_ast
-	tint_lang_wgsl_ast_transform
-	tint_lang_wgsl_common
-	tint_lang_wgsl_features
-	tint_lang_wgsl_helpers
-	tint_lang_wgsl_inspector
-	tint_lang_wgsl_intrinsic
-	tint_lang_wgsl_ir
-	tint_lang_wgsl_program
-	tint_lang_wgsl_reader
-	tint_lang_wgsl_reader_lower
-	tint_lang_wgsl_reader_parser
-	tint_lang_wgsl_reader_program_to_ir
-	tint_lang_wgsl_resolver
-	tint_lang_wgsl_sem
-	tint_lang_wgsl_writer
-	tint_lang_wgsl_writer_ast_printer
-	tint_lang_wgsl_writer_ir_to_program
-	tint_lang_wgsl_writer_raise
-	tint_lang_wgsl_writer_syntax_tree_printer
-	tint_lang_core_common
-	tint_lang_hlsl_writer
-	tint_lang_hlsl_writer_ast_printer
-	tint_lang_hlsl_writer_ast_raise
-	tint_lang_hlsl_writer_printer
-	tint_utils_bytes
-	tint_utils_cli
-	tint_utils_command
-	tint_utils_containers
-	tint_utils_debug
-	tint_utils_diagnostic
-	tint_utils_file
-	tint_utils_generator
-	tint_utils_ice
-	tint_utils_id
-	tint_utils_macros
-	tint_utils_math
-	tint_utils_memory
-	tint_utils_reflection
-	tint_utils_result
-	tint_utils_rtti
-	tint_utils_socket
-	tint_utils_strconv
-	tint_utils_symbol
-	tint_utils_system
-	tint_utils_text
-	tint_utils_traits
-)
-
-foreach (Target ${AllDawnTargets})
-	if (TARGET ${Target})
-		# Is a target...
-		get_property(AliasedTarget TARGET "${Target}" PROPERTY ALIASED_TARGET)
-		if("${AliasedTarget}" STREQUAL "")
-			# ...and is not an alias -> move to the Dawn folder
-			set_property(TARGET ${Target} PROPERTY FOLDER "Dawn")
-		endif()
-	endif()
-endforeach()
-
-# This is likely needed for other targets as well
-# TODO: Notify this upstream (is this still needed?)
-target_include_directories(dawn_utils PUBLIC "${CMAKE_BINARY_DIR}/_deps/dawn-src/src")
