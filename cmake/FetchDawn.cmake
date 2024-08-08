4966371bf472c2588bfd301eb5a70f00220ada4a
Mareks <47260097+sshsphere@users.noreply.github.com>
2024-08-08 18:32:11 +0300

    .

--- cmake/FetchDawn.cmake
+++ cmake/FetchDawn.cmake
@@ -201,3 +201,4 @@ endforeach()
 # This is likely needed for other targets as well
 # TODO: Notify this upstream (is this still needed?)
 target_include_directories(dawn_utils PUBLIC "${CMAKE_BINARY_DIR}/_deps/dawn-src/src")
+export(TARGETS ${AllDawnTargets} FILE dawn-targets.cmake NAMESPACE dawn::)
